ae368ff478ee90c399ce465d24fa6801fd6538fc
jbnilles <jbnilles24@gmail.com>
2020-12-20 13:40:14 -0800

    add message model data type

---
+++ Models/MessageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ph_UserEnv.Models
+{
+    public class MessageModel
+    {
+        
+       
+
+        [Required(ErrorMessage = "User Name is required")]
+        public string sender_id { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string receiver_id { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string message { get; set; }
+    }
+}
