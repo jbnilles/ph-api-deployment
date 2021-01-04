ae55cc81f3e1df7558f89646aafd970153cd79fe
jbnilles <jbnilles24@gmail.com>
2021-01-03 21:46:13 -0800

    testing

--- Startup.cs
+++ Startup.cs
@@ -99,8 +99,11 @@ namespace ph_UserEnv
             
                 app.UseHsts();
                 app.UseSwagger();
-               
-            
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
 
             //app.UseHttpsRedirection();
             //app.UseMvc();
